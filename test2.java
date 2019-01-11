f4a026838687d1aaa3ac0d19fe5ab42103021a73
rudra1111 <rudra.murthy16@gmail.com>
2019-01-11 05:54:59 +0530

    new test2.java added

---
+++ test2.java
@@ -0,0 +1,14 @@
+i have to complete all my taksts 
+i have to one call to my mothere 
+and winfe 
+that is relation point of 
+i have to become the powere of baljinder 
+i have to overcome him 
+with 6 months i have to become greate devops engineer and sql devloper and 
+grate communication speaker 
+
+how is ntr biopic film . sriny with out knowing any thing jhe can speak above file how is it possible 
+
+thabks and regards
+
+
